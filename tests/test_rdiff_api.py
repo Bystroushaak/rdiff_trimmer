dd00350bb642c5cf56a1a58b200e2ad44ce12873
Bystroushaak <bystrousak@kitakitsune.org>
2026-02-10 13:06:17 +0100

    Rewrite to pytest functions & fixtures.

--- tests/test_rdiff_api.py
+++ tests/test_rdiff_api.py
@@ -1,12 +1,7 @@
-#! /usr/bin/env python2
-# -*- coding: utf-8 -*-
-#
-# Interpreter version: python 2.7
-#
 import os
-import shutil
 import os.path
-import tempfile
+
+from pytest import fixture
 
 from rdiff_trimmer.rdiff_api import RdiffAPI
 from rdiff_trimmer.rdiff_api import Increment
@@ -15,41 +10,43 @@ from rdiff_trimmer.rdiff_api import Increment
 DIR_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
-class TestIncrement(object):
-    def test_increment_from_string(self):
-        inc_line = "   1524336818 directory   "
-
-        inc = Increment.from_string(inc_line)
-
-        assert inc.timestamp == 1524336818.0
+@fixture
+def test_data_rdiff_api():
+    return RdiffAPI(os.path.join(DIR_PATH, "test_data"))
 
 
-class TestRdiffAPI(object):
-    def setup(self):
-        self.ra = RdiffAPI(os.path.join(DIR_PATH, "test_data"))
-        self.tmp_dir = tempfile.mkdtemp()
-        self.out_ra = RdiffAPI(self.tmp_dir)
+@fixture
+def tmpdir_rdiff_api(tmp_path):
+    return RdiffAPI(tmp_path)
 
-    def teardown(self):
-        shutil.rmtree(self.tmp_dir)
 
-    def test_yield_increments(self):
-        increments = list(self.ra.yield_increments())
+def test_increment_from_string():
+    inc_line = "   1524336818 directory   "
 
-        assert len(increments) == 3
+    inc = Increment.from_string(inc_line)
 
-        assert increments[1].timestamp == 1524342591.0
-        assert increments[2].timestamp == 1524342605.0
+    assert int(inc.timestamp) == 1524336818
 
-    def test_restore_into(self):
-        increments = list(self.ra.yield_increments())
 
-        self.ra.restore_into(self.tmp_dir, increments_to_keep=[increments[0]])
+def test_yield_increments(test_data_rdiff_api):
+    increments = list(test_data_rdiff_api.yield_increments())
 
-        kept_increments = list(self.out_ra.yield_increments())
+    assert len(increments) == 3
 
-        assert len(kept_increments) == 1
-        assert kept_increments[0].timestamp == increments[0].timestamp
+    assert int(increments[1].timestamp) == 1524342591
+    assert int(increments[2].timestamp) == 1524342605
 
-        assert os.path.exists(os.path.join(self.tmp_dir, "content"))
-        assert not os.path.exists(os.path.join(self.tmp_dir, "and_more_content"))
+
+def test_restore_into(test_data_rdiff_api, tmpdir_rdiff_api):
+    increments = list(test_data_rdiff_api.yield_increments())
+
+    tmp_dir = tmpdir_rdiff_api.rsync_dir
+    test_data_rdiff_api.restore_into(tmp_dir, increments_to_keep=[increments[0]])
+
+    kept_increments = list(tmpdir_rdiff_api.yield_increments())
+
+    assert len(kept_increments) == 1
+    assert kept_increments[0].timestamp == increments[0].timestamp
+
+    assert os.path.exists(os.path.join(tmp_dir, "content"))
+    assert not os.path.exists(os.path.join(tmp_dir, "and_more_content"))
